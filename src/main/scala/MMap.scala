1ce970a56f8082b9920f9229214ac158dba41034
mtumilowicz <michal.tumilowicz01@gmail.com>
2023-02-18 18:08:41 +0100

    draft

--- src/main/scala/MMap.scala
+++ src/main/scala/MMap.scala
@@ -1,10 +1,41 @@
 import cats.Show
 import cats.implicits.showInterpolator
-import io.circe.generic.semiauto.deriveCodec
 import io.circe.syntax.EncoderOps
-import io.circe.{Codec, Decoder, Encoder, Json}
+import io.circe.{Decoder, Encoder, Json}
 
 import scala.collection.mutable
+
+sealed trait Abc[T <: Entity, K <: EntityId, L] {
+  def decode(json: Json)(implicit ev: Decoder[T]): Either[String, T]
+  def showKey: Show[K]
+  def something: L
+}
+
+object Abc {
+  implicit val userAbc = new Abc[User, UserId, String] {
+
+    override def decode(json: Json)(implicit ev: Decoder[User]): Either[String, User] = ev.decodeJson(json) match {
+      case Left(error) => Left(show"Cannot parse json: $error")
+      case Right(value) => Right(value)
+    }
+
+    override def showKey: Show[UserId] = UserId.show
+
+    override def something: String = "test"
+  }
+
+  implicit val accountAbc = new Abc[Account, AccountId, String] {
+
+    override def decode(json: Json)(implicit ev: Decoder[Account]): Either[String, Account] = ev.decodeJson(json) match {
+      case Left(error) => Left(show"Cannot parse json: $error")
+      case Right(value) => Right(value)
+    }
+
+    override def showKey: Show[AccountId] = AccountId.show
+
+    override def something: String = "test"
+  }
+}
 
 
 class MMap() {
@@ -20,7 +51,13 @@ class MMap() {
       case None => Left(show"No entity for id: $id")
     }
 
-  private def parse[T <: Entity : Decoder](json: Json): Either[String, T] =
+  def get2[T <: Entity : Decoder, K <: EntityId : Show, S](id: K)(implicit ev: Abc[T, K, S]): Either[String, T] =
+    map.get(id) match {
+      case Some(json) => ev.decode(json)
+      case None => Left(show"No entity for id: $id")
+    }
+
+  private def parse[T <: Entity : Decoder, K <: EntityId](json: Json): Either[String, T] =
     json.as[T] match {
       case Left(error) => Left(show"Cannot parse json: $error")
       case Right(value) => Right(value)
--- src/test/scala/MMapSpec.scala
+++ src/test/scala/MMapSpec.scala
@@ -1,6 +1,7 @@
+import User.codec
+import Account.codec
 import zio.Scope
 import zio.test.{Spec, TestEnvironment, ZIOSpecDefault, assertTrue}
-
 object MMapSpec extends ZIOSpecDefault {
   override def spec: Spec[TestEnvironment with Scope, Any] = suite("a")(
     test("b") {
@@ -16,6 +17,34 @@ object MMapSpec extends ZIOSpecDefault {
       mmap.put(User(UserId("1")))
 
       assertTrue(mmap.get[User, UserId](UserId("2")) == Left("No entity for id: 2"))
+    },
+    test("c") {
+      val mmap = new MMap()
+
+      mmap.put(User(UserId("1")))
+
+      assertTrue(mmap.get2[User, UserId, String](UserId("1")) == Right(User(UserId("1"))))
+    },
+    test("cc") {
+      val mmap = new MMap()
+
+      mmap.put(User(UserId("1")))
+
+      assertTrue(mmap.get2[User, UserId, String](UserId("2")) == Left("No entity for id: 2"))
+    },
+    test("d") {
+      val mmap = new MMap()
+
+      mmap.put(Account(AccountId("1")))
+
+      assertTrue(mmap.get2[Account, AccountId, String](AccountId("1")) == Right(Account(AccountId("1"))))
+    },
+    test("dd") {
+      val mmap = new MMap()
+
+      mmap.put(Account(AccountId("1")))
+
+      assertTrue(mmap.get2[Account, AccountId, String](AccountId("2")) == Left("No entity for id: 2"))
     }
   )
 }
